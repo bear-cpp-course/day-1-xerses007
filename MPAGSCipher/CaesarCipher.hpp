f598c6b26097e4924a719404b766e993ff4baddb
Paul Teare <pxt517@bham.ac.uk>
2020-03-06 11:30:09 +0000

    copied CipherType from day4 master and included in cmake

--- MPAGSCipher/CaesarCipher.hpp
+++ MPAGSCipher/CaesarCipher.hpp
@@ -6,7 +6,8 @@
 #include "CipherMode.hpp"
 
 /** CaesarCipher class that stores the key and has the applyCipher method that evaluates that key on a given string to either encrypt or decrypt
- * Use it by creating a new cipher and then using .applyCipher on it with the input string and boolean to encrypt **/
+ * Use it by creating a new cipher and then using .applyCipher on it with the input string and boolean to encrypt 
+ */
 class CaesarCipher {
 	public:
 		explicit CaesarCipher(const int key); /// accepts input as int
